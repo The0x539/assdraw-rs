09c2520aae630d0f9438d2fc12ac0a4a421fa514
The0x539 <the0x539@gmail.com>
2021-02-14 16:29:13 -0600

    refactor: start to figure out a way to avoid all the OnceCells

---
+++ src/app.rs
@@ -0,0 +1,250 @@
+use std::cell::Cell;
+use std::rc::Rc;
+
+use native_windows_gui as nwg;
+
+type Canvas = crate::gl::OpenGlCanvas;
+
+fn change_scale(mut scale: f32, factor: i32) -> f32 {
+    assert!(scale > 0.0);
+    if scale < 1.0 {
+        scale = scale.recip().round();
+        scale = -scale + 2.0;
+    }
+
+    scale += factor as f32;
+
+    if scale < 1.0 {
+        scale = (scale - 2.0).abs();
+        scale = scale.recip();
+    }
+    scale
+}
+
+#[derive(Default)]
+pub struct AppBuilder;
+
+pub struct AppInner {
+    window: nwg::Window,
+    #[allow(dead_code)]
+    grid: nwg::GridLayout,
+    pub canvas: Canvas,
+    paste_image_btn: nwg::Button,
+
+    left_dragging: Cell<bool>,
+    right_dragging: Cell<bool>,
+    pre_drag_pos: Cell<[f32; 2]>,
+    drag_start_pos: Cell<(i32, i32)>,
+}
+
+impl AppInner {
+    fn cursor_pos(&self) -> (i32, i32) {
+        nwg::GlobalCursor::local_position(&self.canvas, None)
+    }
+
+    fn is_dragging(&self) -> bool {
+        self.left_dragging.get() || self.right_dragging.get()
+    }
+
+    fn add_point_at_cursor(&self) {
+        let (x, y) = self.cursor_pos();
+        let dims = self.canvas.get_dimensions();
+        let (scene_x, scene_y) = (
+            dims.scene_pos[0] + x as f32 / dims.scale,
+            dims.scene_pos[1] + y as f32 / dims.scale,
+        );
+        self.canvas.add_point(scene_x, scene_y);
+    }
+
+    fn show(&self) {
+        self.window.set_visible(true);
+        self.window.set_focus();
+    }
+    fn resize_canvas(&self) {
+        self.canvas.resize();
+    }
+    fn exit(&self) {
+        nwg::stop_thread_dispatch();
+    }
+    fn zoom(&self, data: nwg::EventData) {
+        let factor = match data {
+            nwg::EventData::OnMouseWheel(i) => i / 120,
+            _ => panic!(),
+        };
+        self.canvas.update_dimensions(|dims| {
+            let (mouse_x, mouse_y) = self.cursor_pos();
+            let [mouse_x, mouse_y] = [mouse_x as f32, mouse_y as f32];
+            let [mouse_scene_x, mouse_scene_y] = [
+                dims.scene_pos[0] + mouse_x / dims.scale,
+                dims.scene_pos[1] + mouse_y / dims.scale,
+            ];
+
+            let new_scale = change_scale(dims.scale, factor);
+
+            let new_scene_pos = [
+                mouse_scene_x - (mouse_x / new_scale),
+                mouse_scene_y - (mouse_y / new_scale),
+            ];
+
+            if self.right_dragging.get() {
+                self.pre_drag_pos.set(new_scene_pos);
+                self.drag_start_pos.set(self.cursor_pos());
+            }
+
+            dims.scale = new_scale;
+            dims.scene_pos = new_scene_pos;
+        })
+    }
+    fn mouse_move(&self) {
+        if !self.is_dragging() {
+            return;
+        }
+
+        let [x0, y0] = self.pre_drag_pos.get();
+        let (dx0, dy0) = self.drag_start_pos.get();
+        let (dx1, dy1) = self.cursor_pos();
+        let (dx, dy) = (dx1 - dx0, dy1 - dy0);
+
+        if self.right_dragging.get() {
+            self.canvas.update_dimensions(|dims| {
+                let x = x0 - (dx as f32) / dims.scale;
+                let y = y0 - (dy as f32) / dims.scale;
+                dims.scene_pos = [x, y];
+            })
+        }
+        if self.left_dragging.get() {
+            self.canvas.pop_point();
+            self.add_point_at_cursor();
+        }
+    }
+    fn mouse_press(&self, event: nwg::MousePressEvent) {
+        let was_dragging = self.is_dragging();
+        match event {
+            nwg::MousePressEvent::MousePressRightDown => {
+                self.right_dragging.set(true);
+            }
+            nwg::MousePressEvent::MousePressRightUp => {
+                self.right_dragging.set(false);
+            }
+            nwg::MousePressEvent::MousePressLeftDown => {
+                self.add_point_at_cursor();
+                self.left_dragging.set(true);
+            }
+            nwg::MousePressEvent::MousePressLeftUp => {
+                self.left_dragging.set(false);
+            }
+        }
+        match (was_dragging, self.is_dragging()) {
+            (false, true) => {
+                nwg::GlobalCursor::set_capture(&self.canvas.handle);
+                self.drag_start_pos.set(self.cursor_pos());
+                self.pre_drag_pos
+                    .set(self.canvas.get_dimensions().scene_pos);
+            }
+            (true, false) => nwg::GlobalCursor::release(),
+            _ => (),
+        }
+    }
+    fn paste_image(&self) {
+        let buf = match clipboard_win::get_clipboard(clipboard_win::formats::Bitmap) {
+            Ok(buf) => buf,
+            e => {
+                println!("{:?}", e);
+                return;
+            }
+        };
+        let cursor = std::io::Cursor::new(&buf[..]);
+        let img = image::codecs::bmp::BmpDecoder::new(cursor).unwrap();
+        self.canvas.set_image(img);
+    }
+}
+
+pub struct App {
+    inner: Rc<AppInner>,
+    handler: nwg::EventHandler,
+}
+
+impl nwg::NativeUi<App> for AppBuilder {
+    fn build_ui(mut data: Self) -> Result<App, nwg::NwgError> {
+        let mut window = Default::default();
+        nwg::Window::builder()
+            .size((600, 500))
+            .position((300, 300))
+            .title("nwg")
+            .flags(nwg::WindowFlags::MAIN_WINDOW)
+            .build(&mut window)?;
+
+        let mut canvas = Canvas::default();
+        nwg::ExternCanvas::builder()
+            .parent(Some(&window))
+            .build(&mut *canvas)?;
+
+        let mut paste_image_btn = Default::default();
+        nwg::Button::builder()
+            .parent(&window)
+            .text("bg")
+            .build(&mut paste_image_btn)?;
+
+        let mut grid = Default::default();
+        nwg::GridLayout::builder()
+            .parent(&window)
+            .max_column(Some(4))
+            .max_row(Some(8))
+            .child_item(nwg::GridLayoutItem::new(&canvas, 0, 0, 3, 8))
+            .child_item(nwg::GridLayoutItem::new(&paste_image_btn, 3, 0, 1, 1))
+            .build(&mut grid)?;
+
+        let inner = Rc::new(AppInner {
+            window,
+            grid,
+            canvas,
+            paste_image_btn,
+
+            left_dragging: Default::default(),
+            right_dragging: Default::default(),
+            pre_drag_pos: Default::default(),
+            drag_start_pos: Default::default(),
+        });
+
+        let ui = Rc::downgrade(&inner);
+        let handle_fn = move |evt, evt_data, handle| {
+            let ui = ui.upgrade().unwrap();
+            use nwg::Event;
+            if handle == ui.window.handle {
+                match evt {
+                    Event::OnInit => ui.show(),
+                    Event::OnResize | Event::OnWindowMaximize => ui.resize_canvas(),
+                    Event::OnWindowClose => ui.exit(),
+                    _ => (),
+                }
+            } else if handle == ui.canvas.handle {
+                match evt {
+                    Event::OnMouseMove => ui.mouse_move(),
+                    Event::OnMouseWheel => ui.zoom(evt_data),
+                    Event::OnMousePress(mouse_evt) => ui.mouse_press(mouse_evt),
+                    _ => (),
+                }
+            } else if handle == ui.paste_image_btn {
+                if evt == Event::OnButtonClick {
+                    ui.paste_image();
+                }
+            }
+        };
+        let handler = nwg::full_bind_event_handler(&inner.window.handle, handle_fn);
+
+        Ok(App { inner, handler })
+    }
+}
+
+impl std::ops::Drop for App {
+    fn drop(&mut self) {
+        nwg::unbind_event_handler(&self.handler);
+    }
+}
+
+impl std::ops::Deref for App {
+    type Target = AppInner;
+    fn deref(&self) -> &Self::Target {
+        &self.inner
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use native_windows_gui as nwg;
 
 use nwg::NativeUi;
 
+mod app;
 mod ass_outline;
 mod canvas;
 mod drawing;
@@ -10,13 +11,23 @@ mod vk;
 
 pub use crate::gl::abstraction;
 
-fn main() {
+fn othermain() {
     nwg::init().expect("Failed to init NWG");
     use canvas::Canvas;
     let app = Canvas::build_ui(Canvas::default()).expect("Failed to build UI");
     app.canvas.create_context();
     app.canvas.render();
 
+    nwg::dispatch_thread_events_with_callback(move || {
+        app.canvas.render();
+    });
+}
+
+fn main() {
+    nwg::init().unwrap();
+    let app = app::AppBuilder::build_ui(Default::default()).unwrap();
+    app.canvas.create_context();
+    app.canvas.render();
     nwg::dispatch_thread_events_with_callback(move || {
         app.canvas.render();
     });
